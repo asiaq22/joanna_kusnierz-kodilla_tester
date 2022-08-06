2c07b516ee8fed40cf286468bab796a24ca3983d
Joanna Kusnierz <asiaq@onet.eu>
2022-08-06 19:33:18 +0200

    selenium ebay test app

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -0,0 +1,20 @@
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleExample {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div")).click();
+
+        WebElement inputField = driver.findElement(By.name("q")); // [4]
+        inputField.sendKeys("Kodilla");
+        inputField.submit();
+    }
+}
